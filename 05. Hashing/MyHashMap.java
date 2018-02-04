36d1e648686c5117382501030cf357f2c4fac63c
giggly <giggly@abv.bg>
2018-02-04 22:12:22 +0200

    Hashing

---
+++ 05. Hashing/MyHashMap.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg05.hashing;
+/**
+ *
+ * @author martin
+ */
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Implementation of a HashMap using a collection of MyLinearMap and
+ * resizing when there are too many entries.
+ *
+ * @author martin
+ * @param <K>
+ * @param <V>
+ *
+ */
+public class MyHashMap<K, V> extends MyBetterMap<K, V> implements Map<K, V> {
+
+	// average number of entries per map before we rehash
+	protected static final double FACTOR = 1.0;
+
+	@Override
+	public V put(K key, V value) {
+		V oldValue = super.put(key, value);
+
+		//System.out.println("Put " + key + " in " + map + " size now " + map.size());
+
+		// check if the number of elements per map exceeds the threshold
+		if (size() > maps.size() * FACTOR) {
+			rehash();
+		}
+		return oldValue;
+	}
+
+	/**
+	 * Doubles the number of maps and rehashes the existing entries.
+	 */
+	/**
+	 *
+	 */
+	protected void rehash() {
+            List<MyLinearMap<K, V>> oldMaps = maps;
+            makeMaps(maps.size() * 2);
+            for (MyLinearMap<K, V> map: oldMaps) {
+		for (Map.Entry<K, V> entry: map.getEntries()) {
+                    put(entry.getKey(), entry.getValue());
+		}
+            }
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Map<String, Integer> map = new MyHashMap<String, Integer>();
+		for (int i=0; i<10; i++) {
+			map.put(new Integer(i).toString(), i);
+		}
+		Integer value = map.get("3");
+		System.out.println(value);
+	}
+}
